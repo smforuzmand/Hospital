4f5829b975a6b853902692ddfdd5d84dfcf322f3
Mohsen Forouzmand <career.forouzmand@gmail.com>
2022-08-05 15:16:31 +0200

    COntactInfoDao implemented

---
+++ src/main/java/dao/implementations/ContactInfoDaoImpl.java
@@ -0,0 +1,60 @@
+package dao.implementations;
+
+import dao.ContactInfoDao;
+import model.ContactInfo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ContactInfoDaoImpl implements ContactInfoDao {
+
+    List<ContactInfo> contactInfoList;
+
+    public ContactInfoDaoImpl() {
+
+        contactInfoList = new ArrayList<>();
+
+    }
+
+    @Override
+    public ContactInfo create(ContactInfo contactInfo) {
+        if (contactInfo == null)
+            throw new RuntimeException("null is not allowed");
+        contactInfoList.add(contactInfo);
+        return contactInfo;
+    }
+
+
+    @Override
+    public Collection<ContactInfo> findAll() {
+        return contactInfoList;
+    }
+
+    @Override
+    public ContactInfo findById(String id) {
+        for (ContactInfo info : contactInfoList){
+            if (info.getId().equals(id)) {
+                return info;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean delete(String id) {
+        return contactInfoList.remove(findById(id));
+    }
+
+    @Override
+    public ContactInfo findByEmail(String fEmail) {
+        for (ContactInfo info : contactInfoList){
+            if (info.getEmail().equals(fEmail)) {
+                return info;
+            }
+        }
+
+        return null;
+    }
+}
